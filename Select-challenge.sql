d203f78412f6c61198ba376162f769ac0808a53c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-07 21:35:02 -0300

    Added DB tar and updated selects script

--- Select-challenge.sql
+++ Select-challenge.sql
@@ -1,2 +1,18 @@
+ -- Grab the first and last names of every customer and
+ -- their email addres
  SELECT first_name,last_name,email  
  FROM customer;
+ 
+ -- Sometimes a table contains a column that has duplicate values, 
+ -- and you may find yourself in a situation 
+ -- where you only want to list t he unique/distinct values.
+ -- The DISTINCT keyword can be used to return only the distinct values in a column
+ SELECT DISTINCT release_year FROM film;
+ SELECT DISTINCT (rental_rate) FROM film;
+ 
+ SELECT DISTINCT rating FROM film
+ 
+ SELECT COUNT (*) FROM payment;
+ SELECT COUNT(amount) FROM payment;
+ SELECT COUNT(DISTINCT(amount))
+ FROM payment;
