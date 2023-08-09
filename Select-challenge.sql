dfaef116b9736ee979106f4a128c180aabbe5394
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-08 23:28:15 -0300

    SELECT exercises with WHERE done

--- Select-challenge.sql
+++ Select-challenge.sql
@@ -16,3 +16,16 @@
  SELECT COUNT(amount) FROM payment;
  SELECT COUNT(DISTINCT(amount))
  FROM payment;
+
+-- A customer forgot their wallet at our store! What is the email for
+-- the customer Nancy Thomas
+SELECT email FROM customer
+WHERE first_name = 'Nancy' AND last_NAME = 'Thomas';
+
+-- Give a description about the movie Outlaw Hanky
+SELECT description FROM film
+WHERE title = 'Outlaw Hanky';
+
+-- Phone number for the customer who lives at 259 Ipoh Drive
+SELECT phone FROM address
+WHERE address = '259 Ipoh Drive';
