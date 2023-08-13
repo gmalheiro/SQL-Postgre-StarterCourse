5dc9aafe05c68c24f0fa6029833c06277ded7637
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-12 23:08:18 -0300

    Select with like

---
+++ Select-Like-3.sql
@@ -0,0 +1,6 @@
+-- The highest customer_id whose name starts with an 'E' and has an address ID lower than 500
+
+SELECT first_name, last_name  FROM customer 
+WHERE first_name LIKE 'E%' AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1;
